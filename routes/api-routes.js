d21f4d9d5ce2e0d32e0708944f92c49d6a154c47
Camden Whirl <camdenwhirl@gmail.com>
2021-06-26 16:52:15 -0600

    Rearranged file structure to remove develop folder

---
+++ routes/api-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const path = require('path');
+let noteData = require('../db/db.json')
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+router.get('/api/notes', (req, res) => res.json(noteData));
+
+    router.post('/api/notes', (req, res) => {
+        let note = req.body;
+        let id = uuidv4();
+        note.id = id;
+
+        noteData.push(note);
+        console.log(noteData);
+        fs.writeFileSync('./db/db.json', JSON.stringify(noteData));
+        res.json(note);
+    });
+
+    router.delete('/api/notes/:id', (req, res) => {
+        let delId = req.params.id;
+        let keepNotes = noteData.filter(note => note.id !== delId);
+        noteData = keepNotes
+
+        fs.writeFileSync('./db/db.json', JSON.stringify(noteData));
+        res.json(noteData);
+    });
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const apiRoutes = require("./Develop/routes/noteRoutes")
-const htmlRoutes = require("./Develop/routes/html-routes")
+const apiRoutes = require("./routes/api-routes")
+const htmlRoutes = require("./routes/html-routes")
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("Develop/public"));
+app.use(express.static("public"));
 app.use("/api", apiRoutes)
 app.use("/", htmlRoutes)
 
