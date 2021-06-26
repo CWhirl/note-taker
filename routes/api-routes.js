07ae2c19991fbc2298a9a8084d150552a6840b2a
Camden Whirl <camdenwhirl@gmail.com>
2021-06-26 17:37:14 -0600

    Filled out readme and added comments explaining my routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,28 +1,29 @@
 const router = require('express').Router();
-let noteData = require('../db/db.json')
+let data = require('../db/db.json')
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
-router.get('/api/notes', (req, res) => res.json(noteData));
+router.get('/api/notes', (req, res) => res.json(data));
+
 
     router.post('/api/notes', (req, res) => {
+        //generates an id for the created note
         let note = req.body;
-        let id = uuidv4();
-        note.id = id;
+        note.id = uuidv4();
 
-        noteData.push(note);
-        console.log(noteData);
-        fs.writeFileSync('./db/db.json', JSON.stringify(noteData));
+        //assigns the id to the note and writes it to the json file
+        data.push(note);
+        fs.writeFileSync('./db/db.json', JSON.stringify(data));
         res.json(note);
     });
 
     router.delete('/api/notes/:id', (req, res) => {
-        let delId = req.params.id;
-        let keepNotes = noteData.filter(note => note.id !== delId);
-        noteData = keepNotes
+        //uses the id from the query paramaters to target the proper note and removes the note from data
+        data = data.filter(note => note.id !== req.params.id);
 
-        fs.writeFileSync('./db/db.json', JSON.stringify(noteData));
-        res.json(noteData);
+        //writes updated note information to the json file
+        fs.writeFileSync('./db/db.json', JSON.stringify(data));
+        res.json(data);
     });
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
-const apiRoutes = require("./routes/api-routes")
-const htmlRoutes = require("./routes/html-routes")
+const api = require("./routes/api-routes")
+const html = require("./routes/html-routes")
 
 const PORT = process.env.PORT || 3001;
 
+//configures express settings
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(apiRoutes)
-app.use(htmlRoutes)
+
+//adds the routes to the express app
+app.use(api);
+app.use(html);
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
